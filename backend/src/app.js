85fc41c76343919ff5bdb0b3ade79bcf1d8fbc4a
chevvuri kishore <Chevvuri Kishore>
2026-09-23 19:26:29 +0530

    Added mongodb connection and socket.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,36 @@
 import express from "express";
-import {createServer} from "node:http";
-
-import {Server} from "socket.io";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import { connectToSocket} from "./controllers/socketManger.js";
 
-const app=express();
-const server=createServer(app);
-const io=new Server(server,)
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.set("port",(process.env.Port|| 8000));
-app.set("")
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({limit : "40kb"}));
+app.use(express.urlencoded({ extended : true, limit:"40kb"}));
 
-app.get("/home",(req,res)=>{
-    return res.json({"hewllo":"world"});
+app.get("/home", (req, res) => {
+  return res.json({ hello: "world" });
 });
 
-const start =async()=>{
+const start = async () => {
+  try{
+     await mongoose.connect("mongodb+srv://kishore:dQRhm5mxnr0cRqmd@cluster0.8jlm4pz.mongodb.net/video_conference?appName=Cluster0")
+     console.log("mongodb connected successfully")
+     server.listen(app.get("port"), () => {
+       console.log(`Listening on port ${app.get("port")}`);
+     });
+  
+  }catch(err){
+    console.log(err);
+  }
     
-    server.listen(app.get("port"),()=>{
-        console.log("listening port 8000");
-    })
-}
+  
+};
 
 start();
